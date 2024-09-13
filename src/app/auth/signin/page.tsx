06dd6f34624f245861a1384ea720b35ac4fde043
Stk <shuheitakahashi0822@gmail.com>
2024-09-13 23:56:22 +0900

    ✨ メールでのログインの追加

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -2,9 +2,12 @@
 
 import { supabase } from "@/lib/supabaseClient";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState<string | null>(null);
   const router = useRouter();
 
   // ログインボタンを押したときのハンドラー
@@ -17,6 +20,19 @@ export default function SignIn() {
     });
     if (error) {
       console.error("Error signing in with Google:", error.message);
+    }
+  };
+  // メールでのログイン
+  const handleMailSignIn = async () => {
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) {
+      setError(error.message); // エラーがあれば表示
+    } else {
+      router.push("/todos"); // ログイン成功後にリダイレクト
     }
   };
 
@@ -53,8 +69,42 @@ export default function SignIn() {
           className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
           onClick={handleSignUpRedirect} // サインアップページに遷移
         >
-          【作成中】アカウント作成フォームを作成中です！
+          メールアドレスでアカウントを作成
         </button>
+        <div className="flex justify-center items-center mt-5">
+          <div className="bg-white p-8 rounded shadow-md w-full max-w-md">
+            <h1 className="text-2xl font-bold mb-6">
+              アカウントをお持ちの方はログイン
+            </h1>
+            {error && <p className="text-red-500 mb-4">{error}</p>}
+            <div className="mb-4">
+              <label className="block text-gray-700 mb-2">メールアドレス</label>
+              <input
+                type="email"
+                className="w-full p-2 border rounded"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="メールアドレス"
+              />
+            </div>
+            <div className="mb-6">
+              <label className="block text-gray-700 mb-2">パスワード</label>
+              <input
+                type="password"
+                className="w-full p-2 border rounded"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="パスワード"
+              />
+            </div>
+            <button
+              className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-700"
+              onClick={handleMailSignIn}
+            >
+              ログイン
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
